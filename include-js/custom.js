2351f82f8857cbde6ab4acc1ba9dd6a78882d82b
Mark David Lee <markadvidlee@outlook.com>
2015-10-15 11:22:25 +0800

    electron bp
    improved UI based on electron best prctices/standard

--- include-js/custom.js
+++ include-js/custom.js
@@ -1,15 +1,24 @@
 $(document).ready(function () {
 	$('.item li').hover(
 		function () {
-			$(this).find('.favorite img').attr("src", "images/star-light.png");
-			$(this).find('.option img:eq(0)').attr("src", "images/share-light.png");
-			$(this).find('.option img:eq(1)').attr("src", "images/password-light.png");
+			$(this).find('.favorite img').attr('src', 'images/star-light.png');
+			$(this).find('.option img:eq(0)').attr('src', 'images/share-light.png');
+			$(this).find('.option img:eq(1)').attr('src', 'images/password-light.png');
 			
 		},
 		function () {
-			$(this).find('.favorite img').attr("src", "images/star-dark.png");
-			$(this).find('.option img:eq(0)').attr("src", "images/share-dark.png");
-			$(this).find('.option img:eq(1)').attr("src", "images/password-dark.png");
+			$(this).find('.favorite img').attr('src', 'images/star-dark.png');
+			$(this).find('.option img:eq(0)').attr('src', 'images/share-dark.png');
+			$(this).find('.option img:eq(1)').attr('src', 'images/password-dark.png');
 			
 		});
+	$('.folders > li > a').click(function(){
+		$(this).parents('li').find('.item').slideToggle();
+		$(this).parents('li').find('img.expand').toggleClass('collapse');
+	});
+	$('.main-menu #show-sidebar').click(function(){
+		$(this).parents('.main-menu').find('.sidebar').toggle();
+		$(this).parents('.main-menu').toggleClass('md-resize');
+		$('.main-content').toggleClass('md-resize');
+	});
 });
